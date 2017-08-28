1ea5897d7c64b9cfd16e9fef60254e685a0a385f
ocket8888 <ocket8888@gmail.com>
2017-08-28 17:22:11 -0600

    handler update

--- test-server.py
+++ test-server.py
@@ -2,16 +2,32 @@
 
 import asyncio
 import websockets
+from logging import getLogger, INFO, StreamHandler
+
+logger = getLogger('websockets')
+logger.setLevel(INFO)
+logger.addHandler(StreamHandler())
+
+clients = set()
 
 async def hello(websocket, path):
-    name = await websocket.recv()
-    print("< {}".format(name))
+    message = await websocket.recv()
+    print("Recieved message: "+repr(message))
 
-    greeting = "Hello {}!".format(name)
+    greeting = "Message Recieved"
     await websocket.send(greeting)
-    print("> {}".format(greeting))
+    print("Sent confirmation of message recipt.")
 
-start_server = websockets.serve(hello, 'localhost', 8765)
+async def handler(websocket, path):
+	global clients
+	clients.add(websocket)
+	try:
+		await asyncio.wait([ws.send("Hello!") for ws in clients])
+		await asyncio.sleep(10)
+	finally:
+		clients.remove(websocket)
+
+start_server = websockets.serve(handler, '127.0.0.1', 6969)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
