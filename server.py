66826f50d54794e939f5d972af4ebf44f2194b8a
ocket8888 <ocket8888@gmail.com>
2019-11-21 01:59:06 -0700

    Began working toward Matrix r0.6.0 compliance

--- server.py
+++ server.py
@@ -1,104 +1,244 @@
 #!/usr/bin/env python3
 
-import asyncio
-import websockets
-from message import Message
-from database import Database
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+import sys
+import typing
+import uuid
+# from message import Message
+# from database import Database
+from lib.auth import renderFlows
+from lib.error import Error, ErrorCode
 
-clients = dict()
+class Client:
+	"""
+	Represents a client session connected to the chat server.
+	"""
+	authenticated = False
+	addr: str
+	def __init__(self, addr: str):
+		self.addr = addr
+
+clients:typing.Dict[str, Client] = {}
 
 #Open a database connection
-try:
-	db = Database("chat")
-except Exception as e:
-	print("[E] Connection to database failed -")
-	print("\t"+str(e))
-	print("[W] Continuing without database...")
-	db = False
+# try:
+# 	db = Database("chat")
+# except Exception as e:
+# 	print("[E] Connection to database failed -")
+# 	print("\t"+str(e))
+# 	print("[W] Continuing without database...")
+# 	db = False
 
+__version__ = "0.0.1"
 
-async def relay(websocket):
-	"""This function waits for messages from every connected client
-	('websocket' is one client connection and the function is called asynchronously for each client)
-	and forwards them to all other clients."""
-	global db
-	while True:
+HOST: str
 
-		#first wait for a message
+class Handler(BaseHTTPRequestHandler):
+	"""
+	The handler for HTTP requests to the chat server. This aims for compliance with Matrix r0.6.0
+	"""
+	server_version = f"chat-server/{__version__}"
+	protocol_version = "HTTP/1.1"
+	error_content_type = "application/json"
+	error_message_format = '{"error": "%(explain)s", "errcode": "%(message)s}\n'
+	_body: str = None
+	_parsedBody: object = None
+
+	def do_GET(self):
+		"""
+		Handles GET requests using the routes configured in self.routes["GET"].
+		"""
 		try:
-			message = await websocket.recv()
-		except:
-			break
-		addr = websocket.remote_address[0]
-		username = clients[addr][1]
-		print("[I] Recieved message from "+addr+" ("+username+")")
+			self.routes["GET"].get(self.path, self.notFoundHandler)()
+		except (Exception, KeyboardInterrupt) as e:
+			print(sys.exc_info, file=sys.stderr)
+			self.send_error(500, ErrorCode.UNKNOWN, e)
 
-		#If it's binary data, it's probably a voip packet, so try that
-		if isinstance(message, bytes):
-			print("it was a voip packet!")
-			for client in clients:
-				#TODO- this: if client != addr:
-				await clients[client][0].send(message)
+	def do_BREW(self):
+		"""
+		Handles BREW requests by informing the client that this server is a teapot, and cannot brew
+		coffee.
+		"""
+		self.send_response(418, "I'm a teapot")
+		self.end_headers()
 
-		#... but if it's a string it's either login info or a text message
-		elif isinstance(message, str):
-			message = Message(message, (addr, username))
+	def authenticated(self) -> bool:
+		"""
+		Returns whether or not the client request was authenticated.
+		"""
+		try:
+			return clients[self.parsedBody["session"]].authenticated
+		except (ValueError, KeyError):
+			return False
 
-			#if it's a login message, store their nickname for later and send them some messages they might've missed.
-			if message.type == "login":
-				
-				clients[websocket.remote_address[0]][1] = message.text
+	def authenticate(self) -> bool:
+		"""
+		Attempts to parse the request body as a supported authentication flow. If it is a supported
+		authentication flow, then it will attempt to perform authentication. Returns whether or not
+		authentication was successful. If authentication *did* fail, this method will have already
+		returned the appropriate error response, and so the caller doesn't need to do anything.
+		"""
+		try:
+			body = self.parsedBody
+		except json.JSONDecodeError as e:
+			self.send_error(400, str(ErrorCode.NOT_JSON), f"Invalid JSON: {e.msg}")
+			return False
+		except ValueError as e:
+			self.send_error(400, str(ErrorCode.UNKNOWN), f"Failed to read request body: {e}")
+			return False
 
-				#send messages to 'em if the database was connected to successfully
-				if db:
-					try:
-						for msg in db.getMessages():
-							await websocket.send(Message.jsonify(("sender", "date", "text"), msg))
-					except Exception as e:
-						del db
-						db = False
+		if "session" not in body or (body["session"] not in clients and "type" not in body):
+			self.unauthorizedHandler()
+			return False
 
+		client = Client(f"{self.client_address[0]}:{self.client_address[1]}")
+		client.authenticated = True
+		clients[body["session"]] = client
+		# For now we'll just blindly accept any login type and credentials.
+		return True
 
-			#if it's a  text message, store it to the database and send it to everyone else
-			elif message.type == "textmsg":
-				for client in clients.values():
-					await client[0].send(message.json())
-				if db:
-					try:
-						db.storeMessage(message)
-					except Exception as e:
-						del db
-						db = False
+	@property
+	def body(self) -> str:
+		"""
+		The request body - unparsed. For a JSON-parsed body, use parsedBody.
+		:raises: ValueError when reading the body fails
+		"""
+		if self._body is None:
+			if "Content-Length" in self.headers:
+				self._body = self.rfile.read(int(self.headers["Content-Length"]))
+			elif "Transfer-Encoding" in self.headers and self.headers["Transfer-Encoding"].lower().strip() == "chunked":
+				chunklength = int(self.rfile.read(3).rstrip("\r\n"))
+				self._body = ""
+				while chunklength >= 0:
+					s = bytearray(chunklength)
+					amt = self.rfile.readinto(s)
+					if amt != chunklength:
+						raise ValueError("Request contained invalid chunked content")
+					self._body += s.decode()
+					chunklength = int(self.rfile.read(4).strip("\r\n"))
 			else:
-				print("[W] Malformed message recieved from "+addr)
-		else:
-			print("[E] Unknown error occured during message receipt from "+addr)
+				#Assume no body
+				self._body = ""
+		return self._body
 
+	@property
+	def parsedBody(self) -> object:
+		"""
+		The object obtained by parsing the request body as JSON.
+		:raises: ValueError when reading or decoding the body fails.
+		"""
+		if self._parsedBody is None:
+			self._parsedBody = json.loads(self.body)
+		return self._parsedBody
 
-async def handler(websocket, path):
-	"""This is called when a client connects to the server.
-	'websocket' is a socket object generated for us that is connected to the client, and 'path' is the client's socket request path.
-	This function handles keeping track of clients' connections and disconnections and passes messages off to 'relay()'"""
-	global clients
-	if websocket.remote_address[0] not in clients:
-		clients[websocket.remote_address[0]] = [websocket, "Unkown"]
-		print("[I] Added a new client - "+websocket.remote_address[0])
+	@property
+	def routes(self) -> typing.Dict[str, typing.Dict[str, typing.Callable]]:
+		"""
+		A mapping of request methods to mappings of request paths to handler methods.
+		"""
+		return {
+			"GET": {
+				"/_matrix/client/versions": self._matrixVersions,
+				"/.well-known/matrix/client": self._wellKnown,
+				"/fakeEndpoint": self._fakeEndpoint,
+			}
+		}
+
+	def _fakeEndpoint(self):
+		print("Handling fake endpoint")
+		if not self.authenticated():
+			print("unauthenticated, attempting to authenticate")
+			if not self.authenticate():
+				print("authentication failed")
+				return
+
+		self.success(b'"authenticated"')
+
+	@property
+	def homeserverBaseURL(self) -> str:
+		"""
+		The home server base_url property which is based on the -H/--host option. Specifically, it's
+		set by the global HOST variable.
+		"""
+		return f'http://matrix.{HOST}'
+
+	@property
+	def identityserverBasURL(self) -> str:
+		"""
+		The identity server base_url property which is based on the -H/--host option. Specifically,
+		it's set by the global HOST variable.
+		"""
+		return f'http://identity.{HOST}'
+
+	def success(self, body: bytes):
+		"""
+		Sends a 200 OK success response with the given body. Sets the Content-Type to
+		application/json, and automatically sets the Content-Length as is appropriate.
+		"""
+		self.send_response(200, "OK")
+		self.send_header("Content-Type", "application/json")
+		self.send_header("Content-Length", str(len(body)))
+		self.end_headers()
+		self.wfile.write(body)
+
+	def _matrixVersions(self):
+		self.success(b'{"versions":["r0.6.0"]}')
+
+	def _wellKnown(self):
+		resp = f'{{"m.homeserver":{{"base_url":"{self.homeserverBaseURL}"}},"m.identity_server":{{"base_url":"{self.identityserverBasURL}"}}}}\n'
+		self.success(resp.encode())
+
+	def notFoundHandler(self):
+		"""
+		Sends a 404 Not Found response with a Matrix-compliant JSON error object body.
+		"""
+		self.send_response(404, "Not Found")
+		self.send_header("Content-Type", "application/json")
+		err = Error("No resource was found for this request", ErrorCode.NOT_FOUND)
+		response = str(err).encode() + b'\n'
+		self.send_header("Content-Length", str(len(response)))
+		self.end_headers()
+		self.wfile.write(response)
+
+	def unauthorizedHandler(self):
+		"""
+		Sends a 401 Unauthorized response with a Matrix-compliant JSON error object body.
+		"""
+		self.send_response(401, "Unauthorized")
+		self.send_header("Content-Type:", "application/json")
+		response = renderFlows(session=str(uuid.uuid5(uuid.NAMESPACE_URL, self.homeserverBaseURL)))
+		self.send_header("Content-Length", str(len(response)))
+		self.end_headers()
+		self.wfile.write(response)
+
+def main() -> int:
+	"""
+	Runs the main logic of the server.
+	Returns an exit code.
+	"""
+	global HOST
+
+	parser = ArgumentParser(description="A simple, Matrix-compliant web server.", formatter_class=ArgumentDefaultsHelpFormatter)
+	parser.add_argument("-p", "--port", type=int, default=6969, help="Set the port on which the server listens")
+	parser.add_argument("-H", "--host", type=str, default="localhost", help="Set the host on which the server listens")
+	parser.add_argument("-v", "--version", action="version", version=__version__)
+	args = parser.parse_args()
+
+	HOST = args.host
+	httpd = HTTPServer((args.host, args.port), Handler)
+	print(f"starting server at http://{args.host}:{args.port}/")
 	try:
-		producer_task = asyncio.ensure_future(relay(websocket))
-		done, pending = await asyncio.wait(
-			[producer_task],
-			return_when=asyncio.FIRST_COMPLETED,
-		)
+		httpd.serve_forever()
+	except Exception as e:
+		print("Halting server:", e, file=sys.stderr)
+		return 1
+	except KeyboardInterrupt as e:
+		print("Halting server", file=sys.stderr)
+		return 1
+	# idk how you get down here
+	return 0
 
-		for task in pending:
-			task.cancel()
-	except:
-		print("[I] User disconnected from "+websocket.remote_address[0])
-	finally:
-		if not clients.pop(websocket.remote_address[0], False):
-			print("[W] Disconnection from unregistered address - something wicked happened...")
-
-start_server = websockets.serve(handler, host='', port=6969)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == "__main__":
+	sys.exit(main())
